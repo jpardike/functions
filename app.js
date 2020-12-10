6454bd94ba0553e245fa6916fc7d8914c1d635af
jpardike <jpardike@gmail.com>
2020-12-09 17:29:13 -0800

    Initial commit

---
+++ app.js
@@ -0,0 +1,79 @@
+// function logAlert() {
+//     console.log('Ah ah ah...You didn\'t say the magic word.');
+// }
+
+// logAlert();
+
+
+// function hello(name) {
+//     return `Hi ${name}`;
+// }
+
+// const greeting = hello('Josh');
+// console.log(greeting);
+
+// hello('Josh');
+
+// function subtract(a, b) {
+//     console.log(a - b);
+// }
+
+// subtract(5, 2);
+
+
+// Make one that adds and one that divides
+
+// function add(a, b) {
+//     console.log(a + b);
+// }
+
+// add(10, 2);
+
+// function divide(a, b) {
+//     console.log(a / b);
+// }
+
+// divide(10, 5);
+
+// add first two numbers than divide
+// convert temp
+
+// function weDoMath(a, b, c) {
+//     return (a + b) / c;
+// }
+
+// const mathResult = weDoMath(10, 5, 3);
+
+// console.log(mathResult);
+
+// Deduct 32, then multiply by 5, then divide by 9
+
+// function convert(num) {
+//     return (num - 32) * 5 / 9;
+// }
+
+// const temp = convert(32);
+
+// console.log(temp);
+
+// take movie title and return quote from that movie
+
+// function quoteGenerator(movie) {
+//     switch (movie) {
+//         case 'Gone With the Wind':
+//             return 'Frankly, my dear, I don\'t give a damn.';
+//             break;
+//         case 'The Godfather':
+//             return 'I\'m going to make him an offer he can\'t refuse.';
+//             break;
+//         default:
+//             return 'I don\'t know that movie.';
+//             break;
+//     }
+// }
+
+// const favMovie = quoteGenerator('Star Wars');
+
+// console.log(favMovie);
+
+
